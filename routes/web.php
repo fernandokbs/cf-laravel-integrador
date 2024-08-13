bba74924c47ebf563fb61e8401b9701120d427fe
Fernando <fernando93d@gmail.com>
2024-08-13 17:32:50 -0600

    UI

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,6 @@ Route::get('/', function () {
 });
 
 Route::resource('products', ProductController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
